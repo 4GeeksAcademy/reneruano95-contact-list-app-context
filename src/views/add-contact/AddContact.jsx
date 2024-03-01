e071094641e6fdae09b50d8a6c31c4271ee0f192
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-03-01 21:05:00 +0000

    changes 2

--- src/views/add-contact/AddContact.jsx
+++ src/views/add-contact/AddContact.jsx
@@ -1,42 +1,85 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import { Button, Col, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Context } from "../../store/appContext";
 
 export const AddContact = () => {
-    return (
+    const { store, actions } = useContext(Context);
+    const [contacts, setContacts] = useState({
+        full_name: '',
+        email: '',
+        address: '',
+        phone: ""
+    })
 
+    const handleSubmit = () => {
+        // e.preventDefault()
+        console.log(contacts)
+        actions.createContact(contacts.full_name, contacts.email, contacts.address, contacts.phone);
+    }
+
+    return (
         <div className="container">
             <h1 className="text-center">Add a new contact</h1>
 
-            <Form>
+            <Form onSubmit={handleSubmit}>
                 <div className="row justify-content-center ">
                     <div className="col-sm-10 mb-2">
-                        <Button variant="secondary" className="d-flex align-items-center justify-content-between">
-                            <p className="ps-1 m-0">Go Back</p>
-                        </Button>
+                        <Link to='/contact'>
+                            <Button variant="secondary" className="d-flex align-items-center justify-content-between">
+                                <p className="ps-1 m-0">Go Back</p>
+                            </Button>
+                        </Link>
                     </div>
 
                     <div className="col-sm-10 col-md-4 mb-2">
                         <Form.Group as={Col} controlId="formGridFullName">
                             <Form.Label>Full Name</Form.Label>
-                            <Form.Control type="text" placeholder="Enter full name" />
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter full name"
+                                value={contacts.full_name}
+                                onChange={(e) => { setContacts({ ...contacts, full_name: e.target.value }) }}
+                                required
+                            />
                         </Form.Group>
                     </div>
                     <div className="col-sm-10 col-md-6 mb-2">
                         <Form.Group as={Col} controlId="formGridEmail">
                             <Form.Label>Email</Form.Label>
-                            <Form.Control type="email" placeholder="Enter email" />
+                            <Form.Control
+                                type="email"
+                                placeholder="Enter email"
+                                value={contacts.email}
+                                onChange={(e) => { setContacts({ ...contacts, email: e.target.value }) }}
+                                required
+                            />
                         </Form.Group>
+
                     </div>
                     <div className="col-sm-10 col-md-4 mb-2">
                         <Form.Group as={Col} controlId="formGridPhone">
-                            <Form.Label>Phone</Form.Label>
-                            <Form.Control type="tel" placeholder="Enter phone number" />
+                            <Form.Label>Phone, format: 123-456-7890</Form.Label>
+                            <Form.Control
+                                type="tel"
+                                placeholder="Enter phone number"
+                                value={contacts.phone}
+                                onChange={(e) => { setContacts({ ...contacts, phone: e.target.value }) }}
+                                pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                                required
+                            />
                         </Form.Group>
                     </div>
                     <div className="col-sm-10 col-md-6 mb-2">
                         <Form.Group as={Col} controlId="formGridAddress">
                             <Form.Label>Address</Form.Label>
-                            <Form.Control type="text" placeholder="Enter address" />
+                            <Form.Control
+                                type="text"
+                                placeholder="Enter address"
+                                value={contacts.address}
+                                onChange={(e) => { setContacts({ ...contacts, address: e.target.value }) }}
+                                required
+                            />
                         </Form.Group>
                     </div>
                     <div className="col-sm-10">
--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -1,18 +1,29 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { ListGroup } from "react-bootstrap";
 
 import { ContactCard } from "../../component/ContactCard";
 import { Context } from "../../store/appContext";
 
 export const Contact = () => {
-    const { store } = useContext(Context)
+    const { store, actions } = useContext(Context)
+    const [newContacts, setNewContacts] = useState({})
+
+    useEffect(() => {
+        setNewContacts(store.contacts);
+    }, [newContacts]);
+
+    const deleteContact = (id) => {
+        actions.deleteContact(id)
+        const newItems = newContacts.filter((item) => item.id !== id);
+        setNewContacts(newItems);
+    };
 
     return (
         <div className="container d-flex justify-content-center">
             {store.contacts ? (
-                store.contacts.map((item, index) => {
-                    return (
-                        <ListGroup className="col-xs-12 col-md-8 col-lg-6 gap-2">
+                <ListGroup className="col-xs-12 col-md-8 col-lg-6 gap-2">
+                    {store.contacts.map((item, index) => {
+                        return (
                             <ContactCard
                                 key={index}
                                 id={item.id}
@@ -20,16 +31,19 @@ export const Contact = () => {
                                 address={item.address}
                                 phone={item.phone}
                                 email={item.email}
+                                onClickDeleteButton={() => {
+                                    deleteContact(item.id)
+                                }}
                             />
-                        </ListGroup >
-                    )
-                })
+                        )
+                    })}
+                </ListGroup >
             ) : (
                 <h1>Loading...</h1>
             )}
-            {store.contacts = [] && (
+            {/* {!store.contacts ? (
                 <h1 className="text-center">Please create a contact</h1>
-            )}
+            ) : ''} */}
         </div >
     )
 }
