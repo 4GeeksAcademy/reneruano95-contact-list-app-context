836e45ce6725d705d3962269eb65dd59c4c6ef21
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2024-03-01 16:40:33 +0000

    changes 1

--- src/views/add-contact/AddContact.jsx
+++ src/views/add-contact/AddContact.jsx
@@ -1,5 +1,52 @@
 import React from "react";
+import { Button, Col, Form } from "react-bootstrap";
 
 export const AddContact = () => {
-    return <div>Add COntatc</div>
+    return (
+
+        <div className="container">
+            <h1 className="text-center">Add a new contact</h1>
+
+            <Form>
+                <div className="row justify-content-center ">
+                    <div className="col-sm-10 mb-2">
+                        <Button variant="secondary" className="d-flex align-items-center justify-content-between">
+                            <p className="ps-1 m-0">Go Back</p>
+                        </Button>
+                    </div>
+
+                    <div className="col-sm-10 col-md-4 mb-2">
+                        <Form.Group as={Col} controlId="formGridFullName">
+                            <Form.Label>Full Name</Form.Label>
+                            <Form.Control type="text" placeholder="Enter full name" />
+                        </Form.Group>
+                    </div>
+                    <div className="col-sm-10 col-md-6 mb-2">
+                        <Form.Group as={Col} controlId="formGridEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email" placeholder="Enter email" />
+                        </Form.Group>
+                    </div>
+                    <div className="col-sm-10 col-md-4 mb-2">
+                        <Form.Group as={Col} controlId="formGridPhone">
+                            <Form.Label>Phone</Form.Label>
+                            <Form.Control type="tel" placeholder="Enter phone number" />
+                        </Form.Group>
+                    </div>
+                    <div className="col-sm-10 col-md-6 mb-2">
+                        <Form.Group as={Col} controlId="formGridAddress">
+                            <Form.Label>Address</Form.Label>
+                            <Form.Control type="text" placeholder="Enter address" />
+                        </Form.Group>
+                    </div>
+                    <div className="col-sm-10">
+                        <Button className="w-100" variant="primary" type="submit">
+                            Submit
+                        </Button>
+                    </div>
+
+                </div >
+            </Form >
+        </div >
+    )
 }
--- src/views/contact/Contact.jsx
+++ src/views/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext } from "react";
 import { ListGroup } from "react-bootstrap";
 
 import { ContactCard } from "../../component/ContactCard";
@@ -10,23 +10,25 @@ export const Contact = () => {
     return (
         <div className="container d-flex justify-content-center">
             {store.contacts ? (
-                <ListGroup className="col-xs-12 col-md-8 col-lg-6 gap-2">
-                    {
-                        store.contacts.map((item, index) => {
-                            return (
-                                <ContactCard
-                                    key={index}
-                                    id={item.id}
-                                    fullName={item.full_name}
-                                    address={item.address}
-                                    phone={item.phone}
-                                    email={item.email}
-                                />)
-                        })
-                    }
-                </ListGroup >
+                store.contacts.map((item, index) => {
+                    return (
+                        <ListGroup className="col-xs-12 col-md-8 col-lg-6 gap-2">
+                            <ContactCard
+                                key={index}
+                                id={item.id}
+                                fullName={item.full_name}
+                                address={item.address}
+                                phone={item.phone}
+                                email={item.email}
+                            />
+                        </ListGroup >
+                    )
+                })
             ) : (
                 <h1>Loading...</h1>
+            )}
+            {store.contacts = [] && (
+                <h1 className="text-center">Please create a contact</h1>
             )}
         </div >
     )
